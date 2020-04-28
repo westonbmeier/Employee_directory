c0a0f3368af6250d05dc25956aad04e25b1d535a
westonbmeier <westonbmeier@gmail.com>
2020-04-27 18:06:07 -0600

    app.use and get and listen are up

--- server.js
+++ server.js
@@ -5,3 +5,15 @@ const publicPath = path.join(__dirname, 'build');
 
 const port = process.env.PORT || 3000;
 
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+
+   res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.listen(port, () => {
+
+   console.log('Server is up and running!');
+
+});
